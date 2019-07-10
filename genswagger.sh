26b45cab625f37375dd4751abbb51e0291b31a71
Eliot Lash <eliot@peartherapeutics.com>
2019-07-10 14:52:06 -0700

    [MP-228] Cleanup genswagger.sh

--- genswagger.sh
+++ genswagger.sh
@@ -2,7 +2,8 @@
 # Regenerates the swagger client using the latest definition file.
 # Note: May cause build breakage if the definition has changed.
 set -o errexit
-# TARGET='vendor/github.com/peartherapeutics/bitrise-step-api-build-trigger'
+set -o nounset
+set -o pipefail
 
 checkForUtility() {
   local UTIL="$1"
@@ -14,7 +15,7 @@ checkForUtility jq
 checkForUtility swagger
 
 set -x
-# Monkey-patch swagger definition to include 201 response to work around strict checks in generated client
+# Monkey-patch swagger definition to remove 200 and include 201 response to work around strict checks in generated client
 curl 'https://api-docs.bitrise.io/docs/swagger.json' | jq 'del(.paths."/apps/{app-slug}/builds".post.responses."200") | .paths."/apps/{app-slug}/builds".post.responses."201" = {"description":"Created","schema":{"type":"object","$ref":"#/definitions/v0.BuildTriggerRespModel"}}' > swagger.json
 rm -Rf models operations client
-swagger generate client #--target="$TARGET"
+swagger generate client
