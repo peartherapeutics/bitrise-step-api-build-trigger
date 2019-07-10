605a6ce0e3d6e63c4f531ae47c1b57cd2da3d817
Eliot Lash <eliot@peartherapeutics.com>
2019-07-10 13:53:48 -0700

    [MP-228] Allow workflow override and add more step outputs

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/go-openapi/strfmt"
@@ -22,11 +23,12 @@ var debugMode bool
 
 // Config ...
 type Config struct {
-	AppSlug string          `env:"triggered_app_slug,required"`
-	Token   stepconf.Secret `env:"bitrise_api_token,required"`
-	Branch  string          `env:"triggered_branch,required"`
-	Message string          `env:"trigger_message"`
-	Verbose bool            `env:"verbose,required"`
+	AppSlug  string          `env:"triggered_app_slug,required"`
+	Token    stepconf.Secret `env:"bitrise_api_token,required"`
+	Branch   string          `env:"triggered_branch,required"`
+	Workflow string          `env:"triggered_workflow"`
+	Message  string          `env:"trigger_message"`
+	Verbose  bool            `env:"verbose,required"`
 }
 
 func main() {
@@ -56,13 +58,14 @@ func main() {
 			BuildParams: &models.V0BuildTriggerParamsBuildParams{
 				CommitMessage: cfg.Message,
 				Branch:        cfg.Branch,
+				WorkflowID:    cfg.Workflow,
 			},
 			HookInfo: &models.V0BuildTriggerParamsHookInfo{
 				Type: "bitrise",
 			},
 		},
 	}
-	buildTriggerParams.SetTimeout(60 * time.Second)
+	buildTriggerParams.SetTimeout(60 * time.Second) // Timeout defaults to 0 :(
 	err := buildTriggerParams.BuildParams.Validate(strfmt.Default)
 	if err != nil {
 		failf("%s", err)
@@ -72,15 +75,16 @@ func main() {
 	resp, err := bitriseClient.Builds.BuildTrigger(&buildTriggerParams, apiKeyQueryAuth)
 
 	if err != nil {
-		log.Errorf("%s", err)
+		failf("%s", err)
 	}
 	log.Infof("\nSuccessfully started build: %#v\n", resp.Payload)
 	// END
 
 	// Output section
-	if err := tools.ExportEnvironmentWithEnvman("BITRISE_API_BUILD_TRIGGER_STEP_RESULT_SLUG", resp.Payload.BuildSlug); err != nil {
-		failf("Failed to generate output - %s, %s", "BITRISE_API_BUILD_TRIGGER_STEP_RESULT_SLUG", err)
-	}
+	exportVar("BITRISE_API_BUILD_TRIGGER_STEP_RESULT_BUILD_SLUG", resp.Payload.BuildSlug)
+	exportVar("BITRISE_API_BUILD_TRIGGER_STEP_RESULT_BUILD_NUMBER", strconv.FormatInt(resp.Payload.BuildNumber, 10))
+	exportVar("BITRISE_API_BUILD_TRIGGER_STEP_RESULT_BUILD_URL", resp.Payload.BuildURL)
+	exportVar("BITRISE_API_BUILD_TRIGGER_STEP_RESULT_TRIGGERED_WORKFLOW", resp.Payload.TriggeredWorkflow)
 
 	// You can find more usage examples on envman's GitHub page
 	//  at: https://github.com/bitrise-io/envman
@@ -100,4 +104,10 @@ func failf(format string, v ...interface{}) {
 	log.Errorf(format, v...)
 	log.Warnf("For more details you can enable the debug logs by turning on the verbose step input.")
 	os.Exit(1)
+}
+
+func exportVar(name, value string) {
+	if err := tools.ExportEnvironmentWithEnvman(name, value); err != nil {
+		failf("Failed to generate output - %s, %s", name, err)
+	}
 }
