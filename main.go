8f0e1a262a2c09eafb9d102dc88f916e55b37bd7
Eliot Lash <eliot@peartherapeutics.com>
2019-07-09 18:07:54 -0700

    [MP-228] Initial commit of go project skeleton
    - Based on https://github.com/bitrise-steplib/steps-appetize-io-deploy
    - Use https://github.com/kardianos/govendor to pull in bitrise go util code

--- main.go
+++ main.go
@@ -1,24 +1,48 @@
 package main
 
 import (
+    "encoding/json"
 	"fmt"
 	"os"
-	"os/exec"
+    "path"
+    "net/url"
+
+    "github.com/bitrise-io/go-utils/log"
+	"github.com/bitrise-tools/go-steputils/stepconf"
+	"github.com/bitrise-tools/go-steputils/tools"
 )
 
+var debugMode bool
+
+// Config ...
+type Config struct {
+	AppPath   string          `env:"app_path,required"`
+	Token     stepconf.Secret `env:"appetize_token,required"`
+	PublicKey string          `env:"public_key"`
+	Verbose   bool            `env:"verbose,required"`
+}
+
 func main() {
+    var cfg Config
+	if err := stepconf.Parse(&cfg); err != nil {
+		failf("Issue with input: %s", err)
+	}
+
+	debugMode = cfg.Verbose
+	log.SetEnableDebugLog(debugMode)
+
+	stepconf.Print(cfg)
+	fmt.Println()
+
+	log.Infof("Checking provided file's platform")
+
 	fmt.Println("This is the value specified for the input 'example_step_input':", os.Getenv("example_step_input"))
 
-	//
-	// --- Step Outputs: Export Environment Variables for other Steps:
-	// You can export Environment Variables for other Steps with
-	//  envman, which is automatically installed by `bitrise setup`.
-	// A very simple example:
-	cmdLog, err := exec.Command("bitrise", "envman", "add", "--key", "EXAMPLE_STEP_OUTPUT", "--value", "the value you want to share").CombinedOutput()
-	if err != nil {
-		fmt.Printf("Failed to expose output with envman, error: %#v | output: %s", err, cmdLog)
-		os.Exit(1)
+    // Output section
+	if err := tools.ExportEnvironmentWithEnvman("VERBOSE", "test exported value"); err != nil {
+		failf("Failed to generate output - %s", "VERBOSE")
 	}
+
 	// You can find more usage examples on envman's GitHub page
 	//  at: https://github.com/bitrise-io/envman
 
@@ -28,4 +52,35 @@ func main() {
 	//  with a 0 exit code `bitrise` will register your Step as "successful".
 	// Any non zero exit code will be registered as "failed" by `bitrise`.
 	os.Exit(0)
+}
+
+// -------------------------------------
+// -- Private methods
+
+func generateAppURL(publicKey string) string {
+	u := url.URL{
+		Scheme: "https",
+		Host:   "appetize.io",
+		Path:   path.Join("app", publicKey),
+	}
+	return u.String()
+}
+
+func failf(format string, v ...interface{}) {
+	log.Errorf(format, v...)
+	log.Warnf("For more details you can enable the debug logs by turning on the verbose step input.")
+	os.Exit(1)
+}
+
+func logDebugPretty(v interface{}) {
+	if !debugMode {
+		return
+	}
+
+	b, err := json.MarshalIndent(v, "", "  ")
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+
+	log.Debugf("Response: %+v\n", string(b))
 }
